d197f1596efc0f692ffcf70c0f99dc3dc647831c
luofuchang <33146051+luofuchang@users.noreply.github.com>
2025-05-05 01:58:10 +0800

    Create work.js

---
+++ work.js
@@ -0,0 +1,236 @@
+// Define main function (script entry)
+
+// 国内DNS服务器
+const domesticNameservers = [
+  "https://dns.alidns.com/dns-query", // 阿里云公共DNS
+  "https://doh.pub/dns-query", // 腾讯DNSPod
+];
+// 国外DNS服务器
+const foreignNameservers = [
+  "https://1.1.1.1/dns-query", // Cloudflare(主)
+  "https://1.0.0.1/dns-query", // Cloudflare(备)
+  "https://208.67.222.222/dns-query", // OpenDNS(主)
+  "https://208.67.220.220/dns-query", // OpenDNS(备)
+  "https://194.242.2.2/dns-query", // Mullvad(主)
+  "https://194.242.2.3/dns-query", // Mullvad(备)
+];
+// DNS配置
+const dnsConfig = {
+  enable: true,
+  listen: "0.0.0.0:1053",
+  ipv6: true,
+  "use-system-hosts": false,
+  "cache-algorithm": "arc",
+  "enhanced-mode": "fake-ip",
+  "fake-ip-range": "198.18.0.1/16",
+  "fake-ip-filter": [
+    // 本地主机/设备
+    "+.lan",
+    "+.local",
+    // Windows网络出现小地球图标
+    "+.msftconnecttest.com",
+    "+.msftncsi.com",
+    // QQ快速登录检测失败
+    "localhost.ptlogin2.qq.com",
+    "localhost.sec.qq.com",
+    // 微信快速登录检测失败
+    "localhost.work.weixin.qq.com",
+  ],
+  "default-nameserver": ["223.5.5.5", "119.29.29.29", "1.1.1.1", "8.8.8.8"],
+  nameserver: [...domesticNameservers, ...foreignNameservers],
+  "proxy-server-nameserver": [...domesticNameservers, ...foreignNameservers],
+  "nameserver-policy": {
+    "geosite:private,cn,geolocation-cn": domesticNameservers,
+    "geosite:google,youtube,telegram,gfw,geolocation-!cn": foreignNameservers,
+  },
+};
+// 规则集通用配置
+const ruleProviderCommon = {
+  type: "http",
+  format: "yaml",
+  interval: 86400,
+};
+// 规则集配置
+const ruleProviders = {
+  proxycidr: {
+    ...ruleProviderCommon,
+    behavior: "ipcidr",
+    url: "https://raw.githubusercontent.com/luofuchang/gfw-rules/refs/heads/master/proxy.txt",
+    path: "./ruleset/loyalsoldier/proxycidr.yaml",
+  },
+  unreachable: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://raw.githubusercontent.com/luofuchang/gfw-rules/refs/heads/master/unreachable.txt",
+    path: "./ruleset/loyalsoldier/unreachable.yaml",
+  },
+  reject: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/reject.txt",
+    path: "./ruleset/loyalsoldier/reject.yaml",
+  },
+  google: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/google.txt",
+    path: "./ruleset/loyalsoldier/google.yaml",
+  },
+  proxy: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/proxy.txt",
+    path: "./ruleset/loyalsoldier/proxy.yaml",
+  },
+  direct: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/direct.txt",
+    path: "./ruleset/loyalsoldier/direct.yaml",
+  },
+  private: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/private.txt",
+    path: "./ruleset/loyalsoldier/private.yaml",
+  },
+  gfw: {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt",
+    path: "./ruleset/loyalsoldier/gfw.yaml",
+  },
+  "tld-not-cn": {
+    ...ruleProviderCommon,
+    behavior: "domain",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/tld-not-cn.txt",
+    path: "./ruleset/loyalsoldier/tld-not-cn.yaml",
+  },
+  telegramcidr: {
+    ...ruleProviderCommon,
+    behavior: "ipcidr",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/telegramcidr.txt",
+    path: "./ruleset/loyalsoldier/telegramcidr.yaml",
+  },
+  lancidr: {
+    ...ruleProviderCommon,
+    behavior: "ipcidr",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/lancidr.txt",
+    path: "./ruleset/loyalsoldier/lancidr.yaml",
+  },
+  applications: {
+    ...ruleProviderCommon,
+    behavior: "classical",
+    url: "https://fastly.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/applications.txt",
+    path: "./ruleset/loyalsoldier/applications.yaml",
+  },
+};
+// 规则
+const rules = [
+  "RULE-SET,google,谷歌服务",
+  // 自定义规则
+  "DOMAIN-SUFFIX,googleapis.cn,节点选择", // Google服务
+  "DOMAIN-SUFFIX,gstatic.com,节点选择", // Google静态资源
+  "DOMAIN-SUFFIX,xn--ngstr-lra8j.com,节点选择", // Google Play下载服务
+  "DOMAIN-SUFFIX,github.io,节点选择", // Github Pages
+  "DOMAIN-SUFFIX,19971214.xyz,节点选择", // Github Pages
+  "DOMAIN,v2rayse.com,节点选择", // V2rayse节点工具
+  // Loyalsoldier 规则集
+  "RULE-SET,proxycidr,节点选择",
+  "RULE-SET,unreachable,全局拦截",
+  "RULE-SET,reject,全局拦截",
+  "RULE-SET,proxy,节点选择",
+  "RULE-SET,direct,全局直连",
+  "RULE-SET,private,全局直连",
+  "RULE-SET,gfw,节点选择",
+  "RULE-SET,tld-not-cn,节点选择",
+  "RULE-SET,applications,全局直连",
+  "RULE-SET,telegramcidr,全局拦截,no-resolve",
+  "RULE-SET,lancidr,全局直连,no-resolve",
+  // 其他规则
+  "GEOIP,LAN,全局直连,no-resolve",
+  "GEOIP,CN,全局直连",
+  "MATCH,漏网之鱼",
+];
+// 代理组通用配置
+const groupBaseOption = {
+  interval: 300,
+  timeout: 3000,
+  url: "https://www.google.com/generate_204",
+  lazy: true,
+  "max-failed-times": 3,
+  hidden: false,
+};
+
+// 程序入口
+function main(config) {
+  const proxyCount = config?.proxies?.length ?? 0;
+  const proxyProviderCount =
+    typeof config?.["proxy-providers"] === "object"
+      ? Object.keys(config["proxy-providers"]).length
+      : 0;
+  if (proxyCount === 0 && proxyProviderCount === 0) {
+    throw new Error("配置文件中未找到任何代理");
+  }
+
+  // 覆盖原配置中DNS配置
+  config["dns"] = dnsConfig;
+
+  // 覆盖原配置中的代理组
+  config["proxy-groups"] = [
+    {
+      ...groupBaseOption,
+      name: "节点选择",
+      type: "select",
+      proxies: ["延迟选优"],
+      "include-all": true,
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/adjust.svg",
+    },
+    {
+      ...groupBaseOption,
+      name: "延迟选优",
+      type: "url-test",
+      tolerance: 100,
+      "include-all": true,
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/speed.svg",
+    },
+    {
+      ...groupBaseOption,
+      name: "谷歌服务",
+      type: "select",
+      proxies: ["节点选择", "延迟选优", "全局直连"],
+      "include-all": false,
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/google.svg",
+    },
+    {
+      ...groupBaseOption,
+      name: "全局直连",
+      type: "select",
+      proxies: ["DIRECT", "节点选择"],
+      "include-all": false,
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/link.svg",
+    },
+    {
+      ...groupBaseOption,
+      name: "全局拦截",
+      type: "select",
+      proxies: ["REJECT", "DIRECT"],
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/block.svg",
+    },
+    {
+      ...groupBaseOption,
+      name: "漏网之鱼",
+      type: "select",
+      proxies: ["节点选择", "延迟选优", "全局直连"],
+      "include-all": false,
+      icon: "https://fastly.jsdelivr.net/gh/clash-verge-rev/clash-verge-rev.github.io@main/docs/assets/icons/fish.svg",
+    },
+  ];
+
+  // 覆盖原配置中的规则
+  config["rule-providers"] = ruleProviders;
+  config["rules"] = rules;
+
+  // 返回修改后的配置
+  return config;
+}
